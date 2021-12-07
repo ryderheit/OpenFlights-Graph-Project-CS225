084117095373c8387a8383e304be57443dbed307
Luke <lukep2@illinois.edu>
2021-12-07 15:58:51 -0600

    added color test output

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,9 +1,13 @@
 #include <vector>
+#include <iostream>
 
 #include "../catch/catch.hpp"
 #include "graph.hpp"
 
 using std::string;
+using std::cout;
+using std::endl;
+
 
 TEST_CASE("test catch", "[weight=1]") {
     int new_int = 35;
@@ -15,7 +19,24 @@ TEST_CASE("test dummy airport", "[weight=5]") {
     auto const airports = "tests/dummy_airports_trivial.txt";
     auto const routes = "tests/dummy_routes_trivial.txt";
     Graph g = Graph(airports, routes);
+
+    cout << "\033[1;34m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[0m\n" << endl;
+    cout << "\033[1;34m        Expected output: \033[0m\n" << endl;
+    cout << "\033[1;34m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[0m\n" << endl;
+    cout << "Starting BFS" << endl;
+    cout << endl;
+    cout << "Total distinct routes in file: " << 2 << endl;
+    cout << "Unexplored edges: " << 0 << endl;
+    cout << "Discovery edges: " << 1 << endl;
+    cout << "Cross edges: " << 1 << endl;
+    cout << "Total distinct routes found in BFS: " << 2 << endl;
+    cout << endl;
+
+    cout << "\033[1;36m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[0m\n" << endl;
+    cout << "\033[1;36m        Actual output: \033[0m\n" << endl;
+    cout << "\033[1;36m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[0m\n" << endl;
     g.BFS();
+    cout << "\033[1;32mEnd of BFS test\033[0m\n" << endl;
 }
 
 TEST_CASE("test path invalid airport", "[weight = 5]"){
