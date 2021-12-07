93710bc0f7dd097283e532cbcfb01c846d28fdbc
Luke <lukep2@illinois.edu>
2021-12-07 16:20:03 -0600

    making another bfs test case

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -15,7 +15,7 @@ TEST_CASE("test catch", "[weight=1]") {
     REQUIRE( 2 + 3 == 5 );
 }
 
-TEST_CASE("test dummy airport", "[weight=5]") {
+TEST_CASE("test dummy airport trivial", "[weight=5]") {
     auto const airports = "tests/dummy_airports_trivial.txt";
     auto const routes = "tests/dummy_routes_trivial.txt";
     Graph g = Graph(airports, routes);
@@ -30,6 +30,30 @@ TEST_CASE("test dummy airport", "[weight=5]") {
     cout << "Discovery edges: " << 1 << endl;
     cout << "Cross edges: " << 1 << endl;
     cout << "Total distinct routes found in BFS: " << 2 << endl;
+    cout << endl;
+
+    cout << "\033[1;36m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[0m\n" << endl;
+    cout << "\033[1;36m        Actual output: \033[0m\n" << endl;
+    cout << "\033[1;36m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[0m\n" << endl;
+    g.BFS();
+    cout << "\033[1;32mEnd of BFS test\033[0m\n" << endl;
+}
+
+TEST_CASE("test dummy airport small", "[weight=5]") {
+    auto const airports = "tests/dummy_airports_trivial.txt";
+    auto const routes = "tests/dummy_routes_small.txt";
+    Graph g = Graph(airports, routes);
+
+    cout << "\033[1;34m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[0m\n" << endl;
+    cout << "\033[1;34m        Expected output: \033[0m\n" << endl;
+    cout << "\033[1;34m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[0m\n" << endl;
+    cout << "Starting BFS" << endl;
+    cout << endl;
+    cout << "Total distinct routes in file: " << 6 << endl;
+    cout << "Unexplored edges: " << 0 << endl;
+    cout << "Discovery edges: " << 2 << endl;
+    cout << "Cross edges: " << 4 << endl;
+    cout << "Total distinct routes found in BFS: " << 6 << endl;
     cout << endl;
 
     cout << "\033[1;36m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[0m\n" << endl;
